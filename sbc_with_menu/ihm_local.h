2888502e8d858bd76228b6c7f4489a3b4d9a8c45
lpaivao <109324115+lpaivao@users.noreply.github.com>
2022-12-10 22:10:54 -0300

    Adição de thread e biblioteca do LCD

---
+++ sbc_with_menu/ihm_local.h
@@ -0,0 +1,13 @@
+#define DP1 4
+#define DP2 17
+#define DP3 27
+#define DP4 22
+
+#define B0 5
+#define B1 19
+#define B2 26
+
+/*Define estados da maquina de estados*/
+#define ESTADO_MENU_DADOS 0
+#define ESTADO_MENU_SOLICITAR 1
+#define ESTADO_MENU_CONFIGURAR 2
--- sbc_with_menu/main.c
+++ sbc_with_menu/main.c
@@ -4,33 +4,64 @@
 #include <MQTTClient.h>
 #include <unistd.h>
 #include <stdbool.h>
-
+#include <wiringPi.h>
+#include <pthread.h>
+/*Biblioteca para a IHM local*/
+#include "ihm_local.h"
 /*Arquivo com as definições de tópicos*/
 #include "../topicos.h"
 #include "../nodeMCU/commands.h"
-
+/*Biblioteca da lista encadeada para armazenamento do historico*/
 #include "lista_encadeada.h"
 
+/*Constantes para confiugração do MQTT*/
 #define IP "tcp://test.mosquitto.org:1883" /*mudar para "tcp://10.0.0.101:1883"*/
-
 #define CLIENTID "MQTTCClientID"
 #define USER "aluno"
 #define PASSWORD "@luno*123"
-
 #define QOS 1
 
+/* Funções da Biblioteca do LCD*/
+/*
+extern void memory_map(void);
+extern void init_lcd(void);
+extern void clear_lcd(void);
+extern void write_char(unsigned char c);*/
+
+/*Inicialização do LCD
+void lcd()
+{
+    memory_map();
+    init_lcd();
+    clear_lcd();
+}*/
+
+/*Mostrar uma string no LCD
+void print_lcd(unsigned char c[])
+{
+    int len = strlen(c);
+
+    for (int i = 0; i < len; i++)
+    {
+        write_char(c[i]);
+    }
+}*/
+
+/*Variaveis Globais*/
 /*Variaveis para configurar mqtt*/
 MQTTClient client;
 int rc;
-
-/*Listas encadeadas para armazenar historico*/
+/*Variaveis de listas encadeadas para armazenar historico*/
 Lista *list_historic_A0;
 Lista *list_historic_D0;
 Lista *list_historic_D1;
 
+/*Delay*/
 char delayTime = 5;
 
+/*Declaracao de funcoes*/
 void publish(MQTTClient client, char *topic, char *payload);
+void *IHM_Local(void *arg);
 
 volatile MQTTClient_deliveryToken deliveredtoken;
 
@@ -69,24 +100,6 @@ void add_medicao_historico(Lista *list, char payloadptr)
     }
 }
 
-/*int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
-{
-    int i;
-    char* payloadptr;
-    printf("Message arrived\n");
-    printf("     topic: %s\n", topicName);
-    printf("   message: ");
-    payloadptr = message->payload;
-    for(i=0; i<message->payloadlen; i++)
-    {
-        putchar(*payloadptr++);
-    }
-    putchar('\n');
-    MQTTClient_freeMessage(&message);
-    MQTTClient_free(topicName);
-    return 1;
-}*/
-
 /*Tratamento da notificação para o sbc pegar os dados dos tópicos que está inscrito pelo primeiro byte do payload, utilizada pela função MQTTClient_setCallbacks();*/
 int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
 {
@@ -119,14 +132,17 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
 
     case NODE_NORMAL:
         printf("Node normal\n");
+        // print_lcd("Node Normal");
         break;
 
     case NODE_SKIP:
         printf("Node skip\n");
+        // print_lcd("Node Skip");
         break;
 
     case NODE_TROUBLE:
         printf("Node trouble\n");
+        // print_lcd("Node Trouble");
         break;
 
     default:
@@ -194,6 +210,7 @@ void mqtt_config()
     printf("MQTT Configurado\n");
 }
 
+/*Submenu de comandos*/
 void menu_comandos()
 {
     int opcao;
@@ -244,34 +261,185 @@ void menu_comandos()
     } while (opcao != 6);
 }
 
-void menu()
+/*Thread Menu Principal*/
+void *menu(void *arg)
 {
-    int opcao;
-    printf("---------Menu---------\n");
-    printf("1 - Menu Comandos\n");
-    scanf("%d", &opcao);
-    system("clear");
-
-    switch (opcao)
+    int opcao = 0;
+    do
     {
-    case 1:
-        menu_comandos();
-        break;
-    default:
-        break;
-    }
+        printf("---------Menu---------\n");
+        printf("0 - Sair\n");
+        printf("1 - Menu Comandos\n");
+        scanf("%d", &opcao);
+        system("clear");
+
+        switch (opcao)
+        {
+        case 0:
+            break;
+        case 1:
+            menu_comandos();
+            break;
+        default:
+            break;
+        }
+    } while (opcao != 0);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-    mqtt_config();
+    // lcd();           /*Primeira configuração do LCD*/
+
+    mqtt_config();   /*Configuração MQTT*/
+    wiringPiSetup(); /*Configuracao do wiringPi*/
+
     /*start_subscribe_topics(client);*/
-    while (true)
-    {
-        menu();
-    }
+
+    pthread_t threadMenu, threadIHM;
+    pthread_create(&threadMenu, NULL, menu, NULL);     // Criacao da thread para o menu
+    pthread_create(&threadIHM, NULL, IHM_Local, NULL); // Criacao da thread para o IHM Local (automatico)
 
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
     return rc;
+}
+
+/*------------------------------------------------- TUDO RELACIONADO AO IHM LOCAL ABAIXO -------------------------------------------------*/
+
+/*Compara a combinação das chaves 3 e 4 atual com a antiga e verifica se o estado mudou*/
+bool state_chaged(int estadoAntigo, int estadoAtual)
+{
+    if (estadoAntigo != estadoAtual)
+    {
+        return false;
+    }
+    return true;
+}
+
+/*Verifica o estado do DIP e retorna o inteiro em decimal referente ao binário da combinação das chaves 3 e 4
+OBS: os botões possuem logica inversa!!*/
+int get_state_dp(int dp3, int dp4)
+{
+    dp3 = digitalRead(DP3);
+    dp4 = digitalRead(DP4);
+    if (dp3 == 1 && dp4 == 1)
+        return 0;
+    else if (dp3 == 1 && dp4 == 0)
+        return 1;
+    else if (dp3 == 0 && dp4 == 1)
+        return 2;
+
+    return -1; // Erro
+}
+
+/*Verifica se o botão do argumento está pressionado*/
+bool isPressed(int Button)
+{
+    if (digitalRead(Button) == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+/*Thread IHM Local*/
+void *IHM_Local(void *arg)
+{
+    /*Definicao dos botoes e chaves como entradas*/
+    pinMode(DP3, INPUT);
+    pinMode(DP4, INPUT);
+    pinMode(B0, INPUT);
+    pinMode(B1, INPUT);
+    pinMode(B2, INPUT);
+
+    int dp3 = 0, dp4 = 0; /*Variavel das chaves*/
+    int estado = 0;       /*Variavel de estado*/
+
+    while (1)
+    {
+        estado = get_state_dp(dp3, dp4);
+        switch (estado)
+        {
+        case ESTADO_MENU_DADOS:
+            // print_lcd("Menu Dados:");
+            while (1)
+            {
+                int estadoNovo = get_state_dp(dp3, dp4);
+                if (state_chaged(estado, estadoNovo))
+                    break;
+                else
+                {
+                    if (isPressed(B0))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    else if (isPressed(B1))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    else if (isPressed(B2))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    /*Nenhum botao pressionado, programa nao faz nada*/
+                }
+                break;
+            }
+        case ESTADO_MENU_SOLICITAR:
+            while (1)
+            {
+                int estadoNovo = get_state_dp(dp3, dp4);
+                if (state_chaged(estado, estadoNovo))
+                    break;
+                else
+                {
+                    if (isPressed(B0))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    else if (isPressed(B1))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    else if (isPressed(B2))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    /*Nenhum botao pressionado, programa nao faz nada*/
+                }
+                break;
+            }
+        case ESTADO_MENU_CONFIGURAR:
+            while (1)
+            {
+                int estadoNovo = get_state_dp(dp3, dp4);
+                if (state_chaged(estado, estadoNovo))
+                    break;
+                else
+                {
+                    if (isPressed(B0))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    else if (isPressed(B1))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    else if (isPressed(B2))
+                    {
+                        // print_lcd("Escreve no lcd");
+                    }
+                    /*Nenhum botao pressionado, programa nao faz nada*/
+                }
+                break;
+            }
+        default:
+            printf("Estado invalido\n");
+            break;
+        }
+        /*Descomentar*/
+        /*clear_lcd();*/
+    }
+    pthread_exit(NULL);
 }
